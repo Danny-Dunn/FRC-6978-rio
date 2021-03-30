d07af2b41a94c441d5135326a67a9d0f512df526
Malcolm Stewart <49325378+Danny-Dunn@users.noreply.github.com>
2021-03-30 19:20:45 -0400

    Increased shooter setpoints and changed button assignments

--- main/java/frc/robot/AlignDriveCamera.java
+++ main/java/frc/robot/AlignDriveCamera.java
@@ -7,7 +7,7 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.RobotBase;
-
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.Joystick;
 
 public class AlignDriveCamera implements Runnable {
@@ -22,6 +22,10 @@ public class AlignDriveCamera implements Runnable {
     long checkIn;
 
     Joystick driveStick;
+
+    public TalonSRX shooterMotor;
+    TalonSRX loaderMotor;
+    TalonSRX intakeLiftMotor;
 
     public AlignDriveCamera(RealTimeDrive RTDrive, Joystick driveStick) {
         this.RTDrive = RTDrive;
@@ -39,6 +43,18 @@ public class AlignDriveCamera implements Runnable {
             tx.setDouble(0.0);
             ty.setDouble(0.0);
         }
+
+        shooterMotor = new TalonSRX(24);
+        loaderMotor = new TalonSRX(25);
+
+        intakeLiftMotor = new TalonSRX(16);
+
+        shooterMotor.setInverted(true);
+        loaderMotor.setInverted(true);
+        shooterMotor.setSensorPhase(false);
+
+        shooterMotor.config_kP(0, 14);
+        //SmartDashboard.putNumber("shooterP", 7);
     }
     
     double calcDistance(double camY) {
@@ -63,29 +79,64 @@ public class AlignDriveCamera implements Runnable {
 
     public boolean exitFlag = false;
     public void run() {
+        //intakeLiftMotor.set(ControlMode.PercentOutput, 0.1);
         exitFlag = false;
         System.out.println("AlignDC: Start");
         //intakeLiftMotor.set(ControlMode.PercentOutput ,0.1);
         while(!exitFlag) {
             long start = System.currentTimeMillis();//marker for exec timing
             
-            if (driveStick.getRawButton(1) && tp.getBoolean(false)) {
+            if (driveStick.getRawButton(6)) {
                 //TODO: Shooter math
                 //RTDrive.shooterInput = this.calcDistance(ty.getDouble(0.0)); //velocity
-                if (!(RTDrive.getShooterVelocity() > RTDrive.shooterInput)) { //semi placeholder, needs target
-                    //TODO: shooter verification
-                } else {
-                    //shooter velocity bad
-                }
+                //shooterMotor.set(ControlMode.Velocity, 2300); //works at 2300 zone 3 actual 2200
+                shooterMotor.set(ControlMode.Velocity, 2700); //testing
+                //shooterMotor.set(conr)
+                
+            } else if (driveStick.getRawButton(2)) {
+                shooterMotor.set(ControlMode.Velocity, 1000);
+            } else if (driveStick.getRawButton(5)) {
+                shooterMotor.set(ControlMode.Velocity, 3150);
+            } else {
+                shooterMotor.set(ControlMode.PercentOutput, 0);
+                
             }
             
-            if (RTDrive.alignEnabled) {
-                //TODO: PID loop for alignment
-                //this needs to be a PID loop based on the 
+            if (driveStick.getRawButton(3)) {
+                //TODO: Shooter math
+                //RTDrive.shooterInput = this.calcDistance(ty.getDouble(0.0)); //velocity
+                loaderMotor.set(ControlMode.PercentOutput, 0.4);
+                
+            } else {
+                loaderMotor.set(ControlMode.PercentOutput, 0);
+                
             }
+
+            if (driveStick.getRawButton(8)) {
+                double error = tx.getDouble(0) - 6.24;
+                SmartDashboard.putNumber("error", error);
+                double gain = 0.045;
+                double gain2 = 0.025;
+                double motorOut;
+
+                if(error > 0.4) {
+                    motorOut = error * gain;
+                } else { 
+                    motorOut = error * gain2;
+                }
+                if(Math.abs(error) < 0.3) {
+                    error = 0;
+                }
+                RTDrive.aimInput = motorOut;
+            }
+
+            SmartDashboard.putNumber("shooterVelocity", shooterMotor.getSelectedSensorVelocity());
+            SmartDashboard.putNumber("shooterCurrent", shooterMotor.getStatorCurrent());
+
             checkIn = System.currentTimeMillis();
             //throws InterruptedException {Thread.sleep(10);}
             if(System.currentTimeMillis() < (start + 1)) try {Thread.sleep(1);} catch (InterruptedException ie) {} //prevents the thread from running too fast
         }
+        intakeLiftMotor.set(ControlMode.PercentOutput, 0);
     }
 }
