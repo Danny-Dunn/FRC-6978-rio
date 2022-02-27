706b3131862e5e23d1230d1e6cfcd676f9dfade7
Danny-Dunn <49325378+Danny-Dunn@users.noreply.github.com>
2022-02-26 21:08:38 -0500

    Add shooter velocity control

--- main/java/frc/robot/Shooter.java
+++ main/java/frc/robot/Shooter.java
@@ -7,49 +7,90 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Shooter implements Runnable, ServiceableModule {
     Joystick driveStick; //joystick(only used for button inputs)
+    int shootButton;
 
     TalonSRX shooterMotor; 
 
     double shooterPower;
+
+    enum ShooterControlMode {
+        velocity,
+        direct,
+        none
+    };
+
+    ShooterControlMode mShooterControlMode;
+
+    public void setShooterControlMode(ShooterControlMode mode) {
+        switch (mode) {
+            case velocity:
+                integralError = 0;
+                integralTS = System.nanoTime();
+                break;
+            default:
+                break;
+        }
+    }
 
     public Shooter(Joystick driveStick) {
         this.driveStick = driveStick;
         SmartDashboard.putNumber("Shooter Power", 0.6);
     }
 
-
-
     public boolean init() {
         shooterMotor = new TalonSRX(10);
         shooterMotor.setSelectedSensorPosition(0);
         shooterMotor.setSensorPhase(true);
-        shooterMotor.setInverted(true);
+        shooterMotor.setInverted(false);
+
+        mShooterControlMode = ShooterControlMode.none;
+
         System.out.println("[Shooter] finished initialisation");
         return true;
     }
 
+    long integralTS;
+    double integralError;
+    double shooterPID(double target, double kP, double kI) {
+        double error = target - shooterMotor.getSelectedSensorVelocity();
+
+        integralError += ((System.nanoTime() - integralTS) * error) / 1000000000;
+        integralTS = System.nanoTime();
+
+        return (error * kP) + (integralError * kI);
+    }
+
     public void standby(boolean takeConfigOptions) {
-        SmartDashboard.putNumber("Shooter Speed", shooterMotor.getSelectedSensorVelocity());
+        SmartDashboard.putNumber("shooterSpeed", shooterMotor.getSelectedSensorVelocity());
+        SmartDashboard.putNumber("Shooter Current", shooterMotor.getStatorCurrent());
 
         if(takeConfigOptions) {
             shooterPower = SmartDashboard.getNumber("Shooter Power", 0.6);
         }
     }
-
+    
     public boolean exitFlag; //this flag is set true when the loop is to be exited
     public void run() {
         exitFlag = false;
         System.out.println("[Shooter] entered independent service");
         while(!exitFlag) {
-            
-            if(driveStick.getRawButton(6)){ 
-                shooterMotor.set(ControlMode.PercentOutput, shooterPower);
-            } //lift button
-            else {
-                shooterMotor.set(ControlMode.PercentOutput, 0.0);
-
+            if(driveStick.getRawButtonPressed(shootButton)) {
+                setShooterControlMode(ShooterControlMode.velocity);
+            } else if(driveStick.getRawButtonReleased(shootButton)) {
+                setShooterControlMode(ShooterControlMode.none);
             }
-            
+
+            switch (mShooterControlMode) {
+                case velocity:
+                    shooterMotor.set(ControlMode.PercentOutput, -shooterPID(19000, 0.000111, 0.0000317));
+                    break;
+                case direct:
+                    shooterMotor.set(ControlMode.PercentOutput, shooterPower);
+                default:
+                    shooterMotor.set(ControlMode.PercentOutput, 0.0);
+                    break;
+            }
+
             try {Thread.sleep(5);} catch (InterruptedException ie) {} //deliberately only updates around 200hz
         }
         System.out.println("[Shooter] left independent service");
