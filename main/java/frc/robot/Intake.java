3290c27fce4b11431d51b09d2ea1e227f0f221a1
Danny-Dunn <49325378+Danny-Dunn@users.noreply.github.com>
2022-02-26 21:10:48 -0500

    Add intake PID control

--- main/java/frc/robot/Intake.java
+++ main/java/frc/robot/Intake.java
@@ -2,75 +2,133 @@ package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-public class Intake implements Runnable{
-    Joystick driveStick; //joystick(only used for button inputs)
+public class Intake implements Runnable, ServiceableModule{
+    Joystick operatorStick; //joystick(only used for button inputs)
 
-    TalonSRX intakeLiftMotor; //retracts ball pickup structure into robot, positive power=lift
-    TalonSRX intakeMotor; //drives rollers to pick up balls, negative power=pull balls in
+    TalonSRX intakeLiftMotor; //retracts ball pickup structure into robot
+    TalonSRX intakeRollerMotor; //drives rollers to pick up cargo
 
-    double catchPoint = -111; //point at which the lift is considered up and should begin bias power
     boolean targetState = true; //determined by joystick buttons
-    boolean realState; //actual state of the lift mechanism
+    double intakeTargetPosition;
 
-    public Intake(Joystick driveStick) {
-        this.driveStick = driveStick;
+    NetworkTable simTable;
+
+    double intakeFullOutPosition = 750;
+    double intakeParkPosition = -25;
+    double intakeTicksPerRadian = 651.8986469;
+    double intakeUpperPosition = -15;
+    double intakeHorizontalBias = 0.14;
+
+    public Intake(Joystick operatorStick) {
+        this.operatorStick = operatorStick;
     }
 
+    public void simOut(String tag, Double value) {
+        if (!RobotBase.isReal()) {
+            simTable.getEntry(tag).setDouble(value);
+        } else {
+            SmartDashboard.putNumber(tag, value);
+        }
+    }
 
+    public boolean init() {
+        intakeLiftMotor = new TalonSRX(30);
+        intakeRollerMotor = new TalonSRX(31);
+        intakeLiftMotor.setSelectedSensorPosition(intakeUpperPosition);
+        if(!RobotBase.isReal()) simTable = NetworkTableInstance.getDefault().getTable("simTable"); //simulation dummy outputs
 
-    public void init() {
-        intakeLiftMotor = new TalonSRX(16);
-        intakeMotor = new TalonSRX(17);
-        intakeLiftMotor.setSelectedSensorPosition(0);
         System.out.println("[Intake] finished initialisation");
+        return true;
     }
 
+    public void standby(boolean takeConfigOptions) {
+        SmartDashboard.putNumber("Intake Lift Radians", intakeLiftMotor.getSelectedSensorPosition() / intakeTicksPerRadian);
+        SmartDashboard.putNumber("Intake Lift Power", intakeLiftMotor.getStatorCurrent() * intakeLiftMotor.getBusVoltage()); //calculate the total power draw in watts
+        simOut("Intake Target Position", intakeTargetPosition);
+    }
+
+    long integralTS;
+    double integralError;
+
+    double intakeLiftPID(double target, double kP) {
+        double error = target - intakeLiftMotor.getSelectedSensorPosition();
+
+        double weightBias = Math.sin(intakeLiftMotor.getSelectedSensorPosition() / intakeTicksPerRadian) * intakeHorizontalBias;
+
+        double movePower = (error * kP);
+        movePower = (movePower > 0.10)? 0.10 : movePower;
+        movePower = (movePower < -0.065)? -0.065 : movePower;
+        SmartDashboard.putNumber("Move Power", movePower);
+        SmartDashboard.putNumber("Intake Error", error);
+
+        return weightBias + movePower;
+    }
 
     public boolean exitFlag; //this flag is set true when the loop is to be exited
     public void run() {
         exitFlag = false;
+        intakeTargetPosition = 0;
+        double deadZone = 0.1;
+        double intakeFineAdjustSpeed = 0.5;
+        long lastCalcTS = System.nanoTime();
+        boolean intakeFineAdjustEnabled = true;
+        boolean rollerFineAdjustEnabled = true;
+        double rollerInSpeed = 0.4;
+        double rollerOutSpeed = -0.2;
         System.out.println("[Intake] entered independent service");
         while(!exitFlag) {
-            
-            //true = up
-            
-            if(driveStick.getRawButton(6)){ 
-                targetState = true;
-                intakeLiftMotor.set(ControlMode.PercentOutput, 0.40);
-            } //lift button
-            else if(driveStick.getRawButton(5)) {
-                intakeLiftMotor.set(ControlMode.PercentOutput, -0.2);
-                targetState = false;
-            } //drop button
-            else {
-                if(targetState) { // go up
-                    intakeLiftMotor.set(ControlMode.PercentOutput, 0.1);
-                    //if(realState) intakeLiftMotor.set(ControlMode.PercentOutput, 0.1); //carriage is up
-                    //else intakeLiftMotor.set(ControlMode.PercentOutput, 0.7);
-                } else { //go down
-                    intakeLiftMotor.set(ControlMode.PercentOutput, -0.1);
-                    //if(realState) intakeLiftMotor.set(ControlMode.PercentOutput, -0.4); //carriage is up
-                    //else if(intakeLiftMotor.getSelectedSensorPosition() < -660) intakeLiftMotor.set(ControlMode.PercentOutput, 0.1); //carriage is all stop(hold)
-                    //else intakeLiftMotor.set(ControlMode.PercentOutput, 0.0); //carriage is downward in motion(slow down)
-                }
-            }
-            realState = targetState;
-            //intakeLiftMotor.set(ControlMode.PercentOutput, 0.1);
-            
-            
+            double deltaT = (System.nanoTime() - lastCalcTS) / 1000000.0d;
+            lastCalcTS = System.nanoTime();
 
-            if(driveStick.getRawButton(4)) {
-                intakeMotor.set(ControlMode.PercentOutput, -0.5);
-            } else if(driveStick.getRawButton(2)) {
-                intakeMotor.set(ControlMode.PercentOutput, 0.5);
-            } else {
-                intakeMotor.set(ControlMode.PercentOutput, 0);
+            if(intakeFineAdjustEnabled) {
+                double y = -operatorStick.getY();
+                y = (y < deadZone && y > -deadZone)? 0 : y;
+                if(y != 0.0) y = (y > 0.0)? y - deadZone : y + deadZone; //eliminate jump behaviour
+                y = y / (1 - deadZone); 
+
+                intakeTargetPosition += y * deltaT * intakeFineAdjustSpeed;
+            }
+
+            intakeTargetPosition = (intakeTargetPosition > intakeFullOutPosition)? intakeFullOutPosition : intakeTargetPosition;
+            intakeTargetPosition = (intakeTargetPosition < intakeParkPosition)? intakeParkPosition : intakeTargetPosition;            
+
+            if(operatorStick.getPOV() == 0) { //set the intake forward
+                intakeTargetPosition = intakeFullOutPosition;
+            } else if(operatorStick.getPOV() == 180) {
+                intakeTargetPosition = intakeParkPosition;
             }
             
-            SmartDashboard.putNumber("Lift Position", intakeLiftMotor.getSelectedSensorPosition());
+            simOut("Intake Power Out", -intakeLiftPID(intakeTargetPosition, 0.0016));
+
+            double liftOut = -intakeLiftPID(intakeTargetPosition, -0.0016);
+            liftOut = (liftOut > 0.19)? 0.19 : liftOut;
+            liftOut = (liftOut < -0.18)? -0.18 : liftOut;
+
+            intakeLiftMotor.set(ControlMode.PercentOutput, liftOut);
+
+            if(rollerFineAdjustEnabled) {
+                double y = operatorStick.getRawAxis(3);
+                y = (y < deadZone && y > -deadZone)? 0 : y;
+                if(y != 0.0) y = (y > 0.0)? y - deadZone : y + deadZone; //eliminate jump behaviour
+                y = y / (1 - deadZone); 
+
+                y = y*0.75;
+
+                simOut("Intake Roller Out", y);
+                intakeRollerMotor.set(ControlMode.PercentOutput, y);
+            } else if(operatorStick.getRawButton(2)) {
+                intakeRollerMotor.set(ControlMode.PercentOutput, rollerInSpeed);
+            } else if(operatorStick.getRawButton(4)) {
+                intakeRollerMotor.set(ControlMode.PercentOutput, rollerOutSpeed);
+            }
+
             try {Thread.sleep(5);} catch (InterruptedException ie) {} //deliberately only updates around 200hz
         }
         System.out.println("[Intake] left independent service");
